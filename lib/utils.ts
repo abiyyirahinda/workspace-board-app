5443433de0ae574a62a2cfb9be571904b5d8b399
abiyyirahinda <muhammadabiyyi@student.ub.ac.id>
2024-02-25 15:43:27 +0700

    room participants

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "indigo", "rose", "fuchsia", "amber", "lime", "emerald", "sky", "blue", "cyan", "green", "pink", "purple", "violet", "yellow", "red",
+]
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length]
 }
